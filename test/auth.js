f29c1ea5145fa78403dbf25808ed15ca5d1c1fe0
edwinwalela <edwinwalela@gmail.com>
2022-06-14 10:18:48 +0300

    hashing unit test

--- test/auth.js
+++ test/auth.js
@@ -1,8 +1,17 @@
 const assert = require('assert');
-const tokenVerification = require("../src/middleware/tokenVerification");
+const {hashPassword} = require("../src/services/auth");
 
-describe("Token Authentication",()=>{
-  it("should be generated",()=>{
+describe("Authentication",()=>{
+
+  it("should hash password",async()=>{
+    let input = {
+      password: "MyStr0nGpAssW()rD!"
+    };
+    let hash = await hashPassword(input);
+    assert.notEqual(input.password,hash)
+  })
+
+  it("should generate token",()=>{
     
   })
 
